b666c88807aef47fede847ab85317b48342e5681
Space06Nick <nikitabezzubov9@gmail.com>
2026-08-04 00:41:04 +0300

    Initial commit: FicArchive.Web MVC project

---
+++ FicArchive.Web/FicArchive.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FicArchive.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
